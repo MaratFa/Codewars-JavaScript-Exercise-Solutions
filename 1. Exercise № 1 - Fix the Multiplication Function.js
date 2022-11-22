9b64ee415e69e00fec47fd44072c7be7236d2479
Marat <80181316+MaratFa@users.noreply.github.com>
2022-11-22 18:57:18 +0300

    Add files via upload

---
+++ 1. Exercise № 1 - Fix the Multiplication Function.js
@@ -0,0 +1,21 @@
+/*
+    -- Description--
+   
+    This code does not execute properly. Try to figure out why:
+        function multiply(a, b){
+            return a * b
+        } 
+   
+*/
+
+
+/*  -- Solution № 1--  */
+
+multiply = function (a, b) {
+  return a * b;
+}
+
+
+/*  -- Solution № 2--  */
+
+multiply = (a, b) => a * b;
