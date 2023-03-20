0df6c158622fe168e871fb0050f8b5c7a6429dac
Marat <80181316+MaratFa@users.noreply.github.com>
2023-03-20 21:03:32 +0300

    Solution

---
+++ 9.0. Exercise № 10 - Get the Middle Character.js
@@ -0,0 +1,24 @@
+/*
+ 
+  -- Description --   
+  
+  You are going to be given a word. Your job is to return the middle character of the word.
+  If the word's length is odd, return the middle character.
+  If the word's length is even, return the middle 2 characters.
+  
+  #Examples:
+  
+  Kata.getMiddle("test") should return "es"
+  Kata.getMiddle("testing") should return "t"
+  Kata.getMiddle("middle") should return "dd"
+  Kata.getMiddle("A") should return "A"
+    
+*/
+
+
+
+/*  -- Solution --  */
+
+function getMiddle(s) {
+  return s.length % 2 ? s.substr((s.length / 2) - 0.5, 1) : s.substr((s.length / 2) - 1, 2);  
+}
